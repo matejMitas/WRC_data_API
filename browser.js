01552209e6557764391119150075a5c2839a423e
Matej Mitas <matej.mitas@gmail.com>
2018-06-15 22:15:32 +0200

    Browser class now has a method for seamless scraping for page content pointed by selector

--- browser.js
+++ browser.js
@@ -4,11 +4,22 @@ module.exports = class Browser {
 	constructor(headless) {
 		this.headless = headless;
 	}
+	// PUBLIC methods
 	async start() {
 		this.browser = await puppeteer.launch({headless: this.headless});
 		this.page = await this.browser.newPage();
 	} 
 	async close() {
 		this.browser.close();
+	}
+	async navigate(path) {
+		await this.page.goto(path);
+	}
+	async crawl(selector) {
+		let status = true;
+		await this.page.waitForSelector(selector, {timeout: 1500}).catch(e => {status = !status});
+		return status ? await this.page.evaluate(selector => {
+	    	return document.querySelector(selector).innerHTML;
+	    }, selector) : undefined;
 	}
 };
