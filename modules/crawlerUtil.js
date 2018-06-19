0adf83088f674ff0f67806ce22eeba52cd916dc2
Matej Mitas <matej.mitas@gmail.com>
2018-06-19 14:37:13 +0200

    Wrapper for page navigation method as well as text extraction tool

--- modules/crawlerUtil.js
+++ modules/crawlerUtil.js
@@ -20,6 +20,11 @@ module.exports = {
 		await this.browser.navigate(url);
 	},
 
+	__navigateClickBrowser: async function(path, selector, prefix) {
+		let url = prefix ? `${this.urls.prefix}${path}` : path;
+		await this.browser.navigateClick(url, selector);
+	},
+
 	__createDate: function(array, offset, normalize) {
 		let len = array.length;
 		if (len < 3 || len > 5) {
@@ -34,6 +39,14 @@ module.exports = {
 			array[3] = array[3] - offset
 
 		return new Date(array[2], array[1] - 1, array[0], array[3] + 1, array[4]);
+	},
+
+	__extractText: function(data) {
+		data = data.split('<br>');
+		data.forEach((item, index) => {
+			data[index] = item.trim();
+		});
+		return data;
 	}
 
 };
