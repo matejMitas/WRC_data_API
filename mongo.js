ec9636590d739f2e9fdd129f984838672e56809b
Matej Mitas <matej.mitas@gmail.com>
2018-06-19 08:17:23 +0200

    Prototyping mongo models

--- mongo.js
+++ mongo.js
@@ -1,34 +1,18 @@
-// getting-started.js
-const mongoose = require('mongoose');
-const dbName = 'wrc_data';
-mongoose.connect(`mongodb://localhost/${dbName}`);
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+// Connection URL
+const url = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'api';
 
-//Define a schema
-var Schema = mongoose.Schema;
-var SomeModelSchema = new Schema({
-    name: String
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+	console.log("Connected successfully to server");
+
+	const db = client.db(dbName);
+
+
+
+	client.close();
 });
-
-// Compile model from schema
-var SomeModel = mongoose.model('SomeModel', SomeModelSchema );
-var awesome_instance = new SomeModel({ name: 'test'});
-// Save the new model instance, passing a callback
-awesome_instance.save(function (err) {
-  if (err) throw err;
-  // saved!
-  console.log('ulozeno');
-});
-
-// find all athletes that play tennis
-var query = SomeModel.find({});
-
-query.exec(function (err, res) {
-  if (err) throw err;
-  // athletes contains an ordered list of 5 athletes who play Tennis
-  console.log(res);
-})
-
-//mongoose.disconnect();
