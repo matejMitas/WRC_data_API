ea5f584151a6cfb64058c7757788406945e43912
Matej Mitas <matej.mitas@gmail.com>
2018-06-24 08:20:06 +0200

    Interface implementation for crawling & saving to DB

--- index.js
+++ index.js
@@ -24,44 +24,126 @@ class Crawler {
 		this.selectors = CrawlerOpt.selectors;
 		this.urls = CrawlerOpt.urls;
 		// temp obj for navigating around the site
-		this.rallies = CrawlerOpt.rallies;
+		this.rallies = undefined;
 	}
 
 	async exec() {
-		// initialize browser
+		// initialize browser & connect to database
 		await this.__openBrowser();
-		// connect to database, clear before use
 		await this.adp.connect();
-		await this.adp.dropCollection('Rallies');
-		
-		// read all rallies
-		//await this.__crawlAllRallies();
-		// read each rally's info
-		for (var key in this.rallies) {
-			// get rally info
-		 	//let timezone = await this.__crawlRallyInfo(key, this.rallies[key].infoLink);
-			// enter results section, first is itinerary, timezone is resolved
-			// for each rally in previous step
-			//await this.__crawlItinerary('results/mexico/stage-times/page/334-228---.html', timezone);
-			await this.adp.insertIntoCollection('Rallies', {acronym: key});
-		}
+		// get basic info
 
-		await this.adp.updateInCollection('Rallies', {}, {info: 'test'}, true);
 
-		//await this.__crawlItinerary('results/mexico/stage-times/page/334-228---.html', -6);
+		//await this.crawlRallyBasic();
+		//await this.crawlItinenaries();
 
-		//console.log(this.rallies);
-		//await this.__crawlLiveText();
-		//await this.__crawlItinerary('results/mexico/stage-times/page/334-228---.html', -5);
-		//await this.__crawlStartList('livetiming/page/4175----.html');
-		//await this.__crawlStageTime();
+		//var payload = await this.__crawlStartList('results/sweden/stage-times/page/326-227---.html');
+		//console.log(JSON.stringify(payload));
+
+		/*for (var item of payload) {
+			var schema = {
+				crewId: item.crewId,
+				members: item.members,
+				equip: [{ 
+					team: item.equip.team, 
+					make: item.equip.make, 
+					car: item.equip.car,
+					// which rallies did the crew used this setup on
+					usedIn: [0] 
+				}],
+				starts: [0]
+			}
+
+			await this.addCrew(schema);
+		}*/
+
+		console.log(await this.adp.findProjectInCollection('Rallies', {}, {acronym: 1}));
+
+
+
+		//await this.adp.updateInCollection('Rallies', {acronym: 'mco'}, {startList: payload}, false);
 
 		// we're done with crawling, bye for now
 		await this.adp.disconnect();
 		await this.__closeBrowser();
 	}
 
-	async crawlFull() {
+	async crawlRallyBasic() {
+		await this.adp.dropCollection('Rallies');
+		await this.__crawlAllRallies();
+
+		for (var key in this.rallies) {
+			let info = await this.__crawlRallyInfo(key, this.rallies[key].infoLink);
+			// update info with links for possible recrawl further on
+			info['links'] = {
+				'detail'	: this.rallies[key].infoLink,
+				'results'	: this.rallies[key].resultsInfo
+			};
+			// save to DB
+			await this.adp.insertIntoCollection('Rallies', {
+				acronym: key.toLowerCase(), 
+				name: this.rallies[key].name,
+				order: this.rallies[key].order,
+				info: info
+			});
+		}
+		// 22.6.2018 - updates for missing wrc.com data
+		await this.adp.updateInCollection('Rallies', {acronym: 'MCO'}, {'info.servicePark': 'Gap'}, false);
+		await this.adp.updateInCollection('Rallies', {acronym: 'GBR'}, {'info.timezone': 1}, false);
+		await this.adp.updateInCollection('Rallies', {acronym: 'FRA'}, {'info.distance': 333.48, 'info.liason': 1120.1}, false);
+	}
+
+	async crawlItinenaries(acronym = false) {
+		var timezones = await this.adp.findProjectInCollection('Rallies', 
+			{}, 
+			{_id: 0, acronym: 1, 'info.timezone': 1, 'info.links.results': 1}
+		);
+
+		for (let i = 0, len = timezones.length; i < len; i++) {
+			let elem = timezones[i];
+
+			if (elem.info.links.results) {
+				console.log(elem.info.links.results);
+				console.log(elem.acronym);
+				console.log(elem.info.timezone);
+
+				var stages = await this.__crawlItinerary(elem.acronym, elem.info.links.results, elem.info.timezone);
+				if (stages.length === 0) {
+					console.log(`Problem for: ${elem.acronym}`)
+				} else {
+					await this.adp.updateInCollection('Rallies', 
+						{acronym: elem.acronym}, 
+						{stages: stages}, 
+						false
+					);
+				}
+			}
+		}
+
+		// normalize itineraries
+		/*var allStages = await this.adp.findProjectInCollection('Rallies', 
+			{},
+			{_id: 0, acronym: 1, 'stages.no': 1, 'stages.name': 1, 'stages.len': 1}
+		);
+
+		for (const rallyStages of allStages) {
+			console.log(rallyStages.acronym);
+
+			for (const stages in rallyStages) {
+				if (stages === 'stages') {
+					for (const stage of rallyStages[stages])  {
+						console.log(`--- ${stage.name}`);
+					}
+				}
+			}
+		}*/
+	}
+
+	async addCrew(crewId) {
+		await this.adp.insertIntoCollection('Crews', crewId);
+	}
+
+	async findCrew(crewId) {
 
 	}
 }
