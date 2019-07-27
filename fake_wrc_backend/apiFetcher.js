64691c2df9a309d3304b84ec3f05fcd2d3e011e4
Matěj Mitaš <matej.mitas@gmail.com>
2019-07-27 10:21:40 +0200

    Rally is not correctly fetch from multiple sources combined into one, human readable ID is added. All fetch item are planned to be logged into database with blueprint working

--- fake_wrc_backend/apiFetcher.js
+++ fake_wrc_backend/apiFetcher.js
@@ -16,33 +16,30 @@ class ApiFetcher {
 	constructor() {
 		this.basePath = config.get('FakeDataApi.originalPath');
 		this.dbConfig = config.get('FakeDataApi.db');
-		/*
-		Each event has two IDs (don't really know why), so 
-		it easier to store them at topmost level.
-		*/
-		this.current = {
-			eventId: 0,
-			rallyId: 0
-		};
 	}
 
 	async execute() {
 		try {
-			var adp = new AdapterModule(
+			this.adp = new AdapterModule(
 				`${this.dbConfig.type}${this.dbConfig.host}:${this.dbConfig.port}`, 
 				this.dbConfig.dbName
 			);
-			await adp.connect();
-
-			this.current.eventId = 81
-			await this._getRallyInfo()
-
+			await this.adp.connect();
+			//await this._getRallyInfo(81);
 			/*
 			First, get all rally IDs (here named eventId to introduce confussion)
 			*/
 			for await (let ralliesData of this._getData()) {
-				//this.current.eventId = ralliesData.eventId
+
+				/*
+				Check if rally is already store in database
+				*/
+
+				console.log(ralliesData.eventId)
 				//await this._getRallyInfo()
+
+				
+
 				/*
 				TODO: 
 					- entries
@@ -52,61 +49,42 @@ class ApiFetcher {
 				*/
 			}
 
-			await adp.disconnect();
-		} catch (error) {
-			console.log(error);
-		}
-	}
-
-	async test() {
-		try {
-			var adp = new AdapterModule(
-				`${this.dbConfig.type}${this.dbConfig.host}:${this.dbConfig.port}`, 
-				this.dbConfig.dbName
-			);
-			await adp.connect();
-
-			var event = 81;
-			var splitIds = [2077, 2078, 2079, 2080, 2082];
-
-			/*
-			Get split IDs
-			*/
-			var splits = (await adp.findProjectInCollection('stages', {'eventId': event, 'stageId': 1031}))[0].splitPoints;
-			var wrcEntries = (await adp.findProjectInCollection('entries', {'eventId': event, 'eligibility': 'M'}));
-
-
-			for (let entry of wrcEntries) {
-				var entryId = entry.entryId;
-				process.stdout.write(`${entry.driver.code}\t`);
-
-				for (let split of splits) {
-					let splitId = split.splitPointId;
-
-					let data = (await adp.findProjectInCollection(
-						'splits', 
-						{'entryId': entryId, 'splitPointId': splitId},
-						{'_id': 0, 'elapsedDurationMs': 1, 'splitDateTime': 1}
-					))[0].elapsedDurationMs;
-
-					process.stdout.write(this._printTime(data).cyan);
-				}
-				process.stdout.write(`\n`);
-			}
-
-			await adp.disconnect();
+			await this.adp.disconnect();
 		} catch (error) {
 			console.log(error);
 		}
 	}
 
 	async _getRallyInfo(eventId) {
-		// let year = parseInt(rallyData.slug.split('-').slice(-1)[0]);
-		// let country = rallyData.countryId;
+		/*
+		Even though 'event' and 'rally' overlaps without
+		any serious thought about consistency we still need
+		both to get all classes
+		*/
+		let eventData = (await this._getData(eventId).next()).value
+		var rallyId = eventData.rallies[0].rallyId
+		let rallyData = (await this._getData(`${eventId}/rallies/${rallyId}`).next()).value
+		/*
+		Add 'eligibilities' and 'groups'
+		*/
+		eventData.eligibilities = rallyData.eligibilities;
+		eventData.groups = rallyData.groups;
+		/*
+		Create unique ID that is human-readable.
+		Extract year and ISO country code
+		*/
+		let payload = {}
+		payload.humanId = {
+			year	: parseInt(eventData.startDate.split('-')[0]),
+			country	: eventData.country.iso3
+		}
+		payload.data = eventData
 
-		let rallyData = (await this._getData(this.current.eventId).next()).value
-		this.current.rallyId = rallyData.rallies[0].rallyId
-		//this.current.rallyId = rallyData
+		console.log(payload);
+	}
+
+	async _getStagesInfo() {
+		//
 	}
 
 	async * _getData(urlModifier=null) {
@@ -126,23 +104,23 @@ class ApiFetcher {
 	  	}
 	}
 
-	_printTime(millis) {
-		let minutes = this._addTrailingZero(Math.round(millis/60000));
-		let seconds = this._addTrailingZero((millis % 60000 / 1000).toFixed(2));
-
-		//let milliseconds =
-
-		return `${minutes}:${seconds}\t`;
+	async _logToDb(payload) {
+		/*
+		Way to track fetching data from API and storing it
+		in our own database
+		*/
+		await this.adp.insertIntoCollection('log', {
+			timestamp: new Date().getTime(),
+			payload: payload
+		});
 	}
 
-	_addTrailingZero(num) {
-		return num < 10 ? `0${num}` : num;
+	async _deleteLogDb(query) {
+		await this.adp.deleteFromCollection('log', query);
 	}
 }
 
 (async function() {
-	const BASEURL = 'https://www.wrc.com/service/sasCacheApi.php?route=events';
-
-	var af = new ApiFetcher('splitsConfig');
-	af.test();
+	var af = new ApiFetcher();
+	af.execute();
 }());
