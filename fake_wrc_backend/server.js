b2fb457720759f6329b2d29cacf5858c3836be4b
Matěj Mitaš <matej.mitas@gmail.com>
2019-07-30 15:02:09 +0200

    Split API mirror done

--- fake_wrc_backend/server.js
+++ fake_wrc_backend/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const app = express();
+const morgan  = require('morgan');
 const AdapterModule = require('../modules/crawler/adapter.js');
+/*
+Config path
+*/
+process.env['NODE_CONFIG_DIR'] = `${__dirname.split('/').slice(0, -1).join('/')}/config`;
+const config = require('config');
+
+const app = express();
+var adp;
+
+app.use(morgan('combined'));
 
 /*
 Info route
@@ -30,20 +40,53 @@ app.get('/events/:eventId', (req, res) => {
 /*
 Split times
 */
-app.get('/events/:eventId/stages/:stageId/splittimes', (req, res) => {
-	res.send(`All info about rally: ${req.params.eventId}`);
-
-	const rally = parseInt(req.query.rallyId);
-	const event = parseInt(req.params.eventId);
+app.get('/events/:eventId/stages/:stageId/splittimes', async (req, res) => {
 	const stage = parseInt(req.params.stageId);
+
+	const splitPointsData = await adp.findProjectInCollection('stage', {'data.stageId': stage}, {'_id': 0, 'data.splitPoints': 1});
+	try {
+		const splitPoints = splitPointsData[0].data.splitPoints.map((item) => item.splitPointId)
+		var ret = await adp.findProjectInCollection('split', 
+			{'data.splitPointId': {'$in': splitPoints}}, 
+			{'_id': 0, 'data': 1}
+		);
+
+		res.json(ret.map((item) => item.data));
+	} catch (err) {
+		res.status(404).send([]);
+	}
+	
+
+	
 });
 
-
+/*
+Error route
+*/
 app.get('*', (req, res) => {  
 	res.status(404).send([]);
 });
 
-app.listen(3000, () => {
-	console.log(`Mirror of 'https://www.wrc.com/service/sasCacheApi.php' running on 3000
-All routes are taken without prefix URL (https://www.wrc.com/service/sasCacheApi.php?route=).`)
-})
+/*
+Wrapped in async function to facilite database adapter's
+asynchronous nature
+*/
+(async function() {
+
+	var dbConfig = config.get('FakeDataApi.db');
+
+	adp = new AdapterModule(
+		`${dbConfig.type}${dbConfig.host}:${dbConfig.port}`, 
+		dbConfig.dbName
+	);
+	await adp.connect();
+
+	const server = app.listen(3000, () => {
+// 	console.log(`Mirror of 'https://www.wrc.com/service/sasCacheApi.php' running on 3000
+// All routes are taken without prefix URL (https://www.wrc.com/service/sasCacheApi.php?route=).`)
+	})
+
+}());
+
+
+
